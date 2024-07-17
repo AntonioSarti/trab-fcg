c92596a8edd1bb7e40a15c7d616c79ef1c1bb545
AntonioSarti <59610410+AntonioSarti@users.noreply.github.com>
2024-07-17 20:56:41 -0300

    Add files via upload

---
+++ build/src/CMakeFiles/trab-fcg.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "../bin/trab-fcg.exe"
+  "../bin/trab-fcg.exe.manifest"
+  "../bin/trab-fcg.pdb"
+  "../lib/libtrab-fcg.dll.a"
+  "CMakeFiles/trab-fcg.dir/main.cpp.obj"
+  "CMakeFiles/trab-fcg.dir/main.cpp.obj.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/trab-fcg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/src/CMakeFiles/trab-fcg.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for trab-fcg.
+# This may be replaced when dependencies are built.
